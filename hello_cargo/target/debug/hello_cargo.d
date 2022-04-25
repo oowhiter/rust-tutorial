6c639e227da0559161ceae0afc411b196b9e9cb1
oowhiter <11532057+oowhiter@users.noreply.github.com>
2022-04-26 00:40:21 +0900

    Finish section 1

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/oowhiter/repo/rust-tutorial/hello_cargo/target/debug/hello_cargo: /home/oowhiter/repo/rust-tutorial/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/oowhiter/repo/rust-tutorial/hello_cargo/target/release/hello_cargo: /home/oowhiter/repo/rust-tutorial/hello_cargo/src/main.rs
